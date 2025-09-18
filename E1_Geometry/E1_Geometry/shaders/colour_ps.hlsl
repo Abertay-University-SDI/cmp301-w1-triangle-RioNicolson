e1333f319633eb8de412e3fed076842d14694996
Rio_Nicolson <2300314@uad.ac.uk>
2025-09-18 15:08:55 +0100

    works jankily

--- E1_Geometry/E1_Geometry/shaders/colour_ps.hlsl
+++ E1_Geometry/E1_Geometry/shaders/colour_ps.hlsl
@@ -4,11 +4,12 @@
 struct InputType
 {
 	float4 position : SV_POSITION;
-	float4 colour : COLOR;
+    float4 colour : COLOR ;
 };
 
 
 float4 main(InputType input) : SV_TARGET
 {
-	return input.colour;
+	//return input.colour;----------------------------------------set to red dont know how to edit the input colour properly
+    return float4(1, 0, 0, 1);
 }
